d586ac2a5577488546bdad8f8b16620efb393915
AlexVinchi <alex.sudzilovkiy@gmail.com>
2023-01-18 15:26:19 +0200

    Initial commit

---
+++ src/Loader.java
@@ -0,0 +1,63 @@
+
+public class Loader
+{
+    public static void main(String[] args)
+    {
+        Cat catFirst = new Cat();
+
+        System.out.println("First cat weight:" + catFirst.getWeight());
+
+        double feed = 10;
+        catFirst.feed(feed);
+        System.out.println("First cat weight:" + catFirst.getWeight());
+
+        int i = 0;
+        int m = 1;
+
+        while (catFirst.getStatus() != "Dead"){
+            i++;
+            if (i == 100){
+                System.out.println(catFirst.meow() + " count meow:" + m * 100);
+                m++;
+                i = 0;
+            }else catFirst.meow();
+        }
+
+        System.out.println("First cat weight:" + catFirst.getWeight());
+        System.out.println("First cat status:" + catFirst.getStatus());
+        System.out.println("=================================================");
+
+
+        var catSecond = new Cat();
+
+        System.out.println("Second cat weight:" + catSecond.getWeight());
+
+
+        catFirst.feed(feed);
+        System.out.println("Second cat weight:" + catSecond.getWeight());
+
+        i = 0;
+        m = 1;
+        feed = 100;
+
+        while (catSecond.getStatus() != "Exploded"){
+            i++;
+            if (i == 100){
+                catSecond.feed(feed);
+                System.out.println("count feed:" + m * 10);
+                m++;
+                i = 0;
+            }else catSecond.feed(feed);
+        }
+        System.out.println("Second cat weight:" + catSecond.getWeight());
+        System.out.println("Second cat status:" + catSecond.getStatus());
+
+
+
+        /*
+        var cat = new Cat();
+
+        System.out.println(cat.getStatus());
+        */
+    }
+}
